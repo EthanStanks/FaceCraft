9e78f16eb9c13b90f371189b747e8b63d96c77df
Ethan <93112834+EthanStanks@users.noreply.github.com>
2024-07-19 16:54:48 -0500

    Update app.py

--- App/app.py
+++ App/app.py
@@ -22,16 +22,20 @@ def get_checkpoint(checkpoint_dropdown):
 
 def generate_prompt(prompt, num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox, checkpoint_dropdown):
     gen, mapping_network = get_checkpoint(checkpoint_dropdown)
-    return generator.generate_from_prompt(prompt, num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox, gen, mapping_network)
+    return generator.generate_from_prompt(prompt, num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox, gen, mapping_network), gr.update(visible=True)
 
 def generate_random(num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox, checkpoint_dropdown):
     gen, mapping_network = get_checkpoint(checkpoint_dropdown)
-    return generator.generate_random_images(num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox,gen, mapping_network)
+    return generator.generate_random_images(num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox,gen, mapping_network), gr.update(visible=True)
 
 def generate_from_image(image, num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox, checkpoint_dropdown):
     gen, mapping_network = get_checkpoint(checkpoint_dropdown)
     resized_image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_AREA)
-    return generator.image_to_image(resized_image, num_images_dropdown,resolution_dropdown,filter_checkbox, head_checkbox, gen, mapping_network)
+    return generator.image_to_image(resized_image, num_images_dropdown,resolution_dropdown,filter_checkbox, head_checkbox, gen, mapping_network), gr.update(visible=True)
+
+def manipulate_space(scale,num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox,checkpoint_dropdown):
+    gen, mapping_network = get_checkpoint(checkpoint_dropdown)
+    return generator.manipulate_imgs(scale, num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox, gen)
 
 with gr.Blocks(title="FaceCraft",css=css) as demo:
     gr.HTML("""
@@ -41,29 +45,29 @@ with gr.Blocks(title="FaceCraft",css=css) as demo:
                 </header>
             """)
 
-    with gr.Row():
-        gallery = gr.Gallery(interactive=False)
+    gallery = gr.Gallery(interactive=False)
+    slider = gr.Slider(minimum=-5.0, maximum=5.0,value=0.0,step=0.1, label="Manipulate Image Latent Space", visible=False, interactive=True)
 
     label = gr.Label("Click \"Generate\" to generate a realistic face.", scale=0,show_label=False)
     with gr.Accordion("Settings"):
         with gr.Row():
-            num_images_dropdown = gr.Dropdown(choices=[1, 2, 3, 4], label="Number of Images to Generate", value=1, interactive=True)
+            num_images_dropdown = gr.Dropdown(choices=[1, 2, 3, 4], label="Change Number of Images", value=1, interactive=True)
             checkpoint_dropdown = gr.Dropdown(choices=["epoch 1", "epoch 40", "epoch 86","epoch 120","epoch 140"], label="Change Training Checkpoint", value="epoch 140", interactive=True)
-            resolution_dropdown = gr.Dropdown(choices=['256x256','512x512', '1024x1024', 'all'], label="Image Resolution to Generate", value='256x256', interactive=True)
-            filter_checkbox = gr.CheckboxGroup(["Cartoon", "Pencil Sketch", "Watercolor", "Xray", "Black and White", "Sepia", "Blue Tone"], label="Apply Filters")
-            head_checkbox = gr.Checkbox(label="Transparent Background", value=False)
+            resolution_dropdown = gr.Dropdown(choices=['256x256','512x512', '1024x1024', 'all'], label="Change Image Resolution", value='256x256', interactive=True)
+            filter_checkbox = gr.CheckboxGroup(["Cartoon", "Pencil Sketch", "Watercolor", "Xray", "Black and White", "Sepia", "Blue Tone"], label="Enable Filters")
+            head_checkbox = gr.Checkbox(label="Enable Transparent Background", value=True)
 
     prompt_input = gr.Textbox(lines=1, placeholder="Your prompt...", visible=True, show_label=False)
     with gr.Row():
         btn_prompt = gr.Button("Prompt Generate")
         btn_random = gr.Button("Random Generate")
-        btn_prompt.click(fn=generate_prompt, inputs=[prompt_input,num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox, checkpoint_dropdown], outputs=[gallery])
-        btn_random.click(fn=generate_random, inputs=[num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox, checkpoint_dropdown], outputs=[gallery])
+        btn_prompt.click(fn=generate_prompt, inputs=[prompt_input,num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox, checkpoint_dropdown], outputs=[gallery,slider])
+        btn_random.click(fn=generate_random, inputs=[num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox, checkpoint_dropdown], outputs=[gallery,slider])
 
     image_label = gr.Label("Upload an image to generate a realistic face.", scale=0,show_label=False)
     input_image = gr.Image(show_download_button=False, scale=0,show_label=False)
-    input_image.upload(fn=generate_from_image, inputs=[input_image,num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox, checkpoint_dropdown], outputs=[gallery])
-
+    input_image.upload(fn=generate_from_image, inputs=[input_image,num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox, checkpoint_dropdown], outputs=[gallery,slider])
+    slider.input(fn=manipulate_space, inputs=[slider,num_images_dropdown, resolution_dropdown, filter_checkbox, head_checkbox,checkpoint_dropdown], outputs=[gallery])
     gr.HTML("""
                 <footer class="custom-footer">
                     <p>Developed By</p>
